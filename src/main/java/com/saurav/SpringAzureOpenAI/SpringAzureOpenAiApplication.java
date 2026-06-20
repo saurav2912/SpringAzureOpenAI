4a47177df43681b8ebdb5db7e13abc7332d119b8
saurav2912 <saurav.saurav08@gmail.com>
2026-06-20 17:08:25 +0530

    Initial Commit

---
+++ src/main/java/com/saurav/SpringAzureOpenAI/SpringAzureOpenAiApplication.java
@@ -0,0 +1,13 @@
+package com.saurav.SpringAzureOpenAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAzureOpenAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzureOpenAiApplication.class, args);
+	}
+
+}
