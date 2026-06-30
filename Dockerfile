240e1a4cde4b04892b161092e7b58fadb1f0401b
saurav2912 <saurav.saurav08@gmail.com>
2026-06-30 09:23:03 +0530

    commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM maven:3.9.11-eclipse-temurin-25 AS builder
+WORKDIR /app
+COPY pom.xml .
+COPY src ./src
+RUN mvn clean package -DskipTests
+
+FROM eclipse-temurin:25-jre
+WORKDIR /app
+COPY target/*.jar app.jar
+EXPOSE 8080
+ENTRYPOINT ["java", "-jar", "app.jar"]
---
+++ src/main/java/com/saurav/SpringAzureOpenAI/AI200Controller.java
@@ -0,0 +1,14 @@
+package com.saurav.SpringAzureOpenAI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AI200Controller {
+
+    @GetMapping("/ai200")
+    public ResponseEntity<String> getAI200() {
+        return ResponseEntity.ok("AI200 is running!");
+    }
+}
