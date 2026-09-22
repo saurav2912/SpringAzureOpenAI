e689e9f14b99a371b30b288848643b06ace119a7
saurav2912 <saurav.saurav08@gmail.com>
2026-09-22 08:54:29 +0530

    commit

--- src/main/java/com/saurav/SpringAzureOpenAI/AI200Controller.java
+++ src/main/java/com/saurav/SpringAzureOpenAI/AI200Controller.java
@@ -10,7 +10,7 @@ public class AI200Controller {
     @GetMapping("/ai200")
     public ResponseEntity<String> getAI200() {
 
-        return ResponseEntity.ok("AI200 is running in Azure Container Apps with Redis!!!");
+        return ResponseEntity.ok("AI200 is running in Azure Container Apps with Redis on 22 Sep 8.30 AM!!!");
     }
 
     @GetMapping("/cpu")
